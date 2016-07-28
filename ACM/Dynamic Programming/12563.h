df8b9d0a5a51efbf9f8ca92efa63292341f75483
Robert <robert.chen.kfc@gmail.com>
2016-07-28 11:02:25 +0800

    12563 changed

--- ACM/Dynamic Programming/12563.h
+++ ACM/Dynamic Programming/12563.h
@@ -1,73 +1,94 @@
 #include <iostream>
 #include <cstring>
+#include <algorithm>
 #include <climits>
 
 using namespace std;
 
-const int MAX_SONGS = 55, MAX_TIME = 10000;
-const int JIN_GE_JIN_QU_TIME = 678;
+struct Song {
+	int length;
+	int value;
+};
+
+struct Knapsack {
+	int length;
+	int value;
+};
+
+const int MAX_SONGS = 55, MAX_TIME_LIM = 10000;
+const int LONG_SONG_TIME = 678;
+const Knapsack EMPTY = { 0, 0 };
+
+#define ABS(X) ((X) > 0 ? (X) : -(X))
 
 int main() {
-	int songs[MAX_SONGS], times[MAX_TIME];
-	bool table[MAX_SONGS][MAX_TIME];
-	bool chosen[MAX_SONGS];
+	Song songs[MAX_SONGS];
+	Knapsack arr[MAX_TIME_LIM];
+	bool map[MAX_SONGS][MAX_TIME_LIM];
+	int num = 1;
 
 	int numCases;
 	cin >> numCases;
-	for(int caseNum = 1; caseNum <= numCases; caseNum++) {
+	while(numCases--) {
 		int numSongs, timeLim;
 		cin >> numSongs >> timeLim;
-		int songTime = 0;
-		for(int i = 0; i < numSongs; i++)
-			cin >> songs[i], songTime += songs[i];
+		for(int i = 0; i < numSongs; i++) {
+			cin >> songs[i].length;
+			songs[i].value = 1;
+		}
 
-		if(songTime + JIN_GE_JIN_QU_TIME <= timeLim)
-			cout << "Case " << caseNum << ": " << numSongs + 1 << " " << songTime + JIN_GE_JIN_QU_TIME << endl;
-		else {
-			memset(times, 0, sizeof(times));
-			for(int i = 0; i < numSongs; i++)
-				memset(table[i], false, sizeof(table[i]));
-			for(int i = 0; i < numSongs; i++) {
-				for(int j = timeLim; j >= songs[i]; j--) {
-					if(times[j - songs[i]] + songs[i] > times[j]) {
-						times[j] = times[j - songs[i]] + songs[i];
-						table[i][j] = true;
-					}
+		for(int i = 0; i <= timeLim; i++)
+			arr[i] = EMPTY;
+		for(int i = 0; i < MAX_SONGS; i++)
+			memset(map[i], false, sizeof(map[i]));
+		for(int i = 0; i < numSongs; i++) {
+			for(int j = timeLim; j >= songs[i].length; j--) {
+				if(arr[j - songs[i].length].value + songs[i].value > arr[j].value) {
+					arr[j].value = arr[j - songs[i].length].value + songs[i].value;
+					arr[j].length = arr[j - songs[i].length].length + songs[i].length;
+					map[i][j] = true;
+				}
+				else if((arr[j - songs[i].length].value + songs[i].value == arr[j].value) &&
+						(arr[j - songs[i].length].length + songs[i].length > arr[j].length)) {
+					arr[j].value = arr[j - songs[i].length].value + songs[i].value;
+					arr[j].length = arr[j - songs[i].length].length + songs[i].length;
+					map[i][j] = true;
 				}
 			}
+		}
 
-			// test
-			/*
-			cout << "table: " << endl;
-			for(int i = 0; i < numSongs; i++) {
-				for(int j = 0; j < timeLim; j++)
-					cout << table[i][j] << " ";
-				cout << endl;
-			}
-			*/
-
-			memset(chosen, false, sizeof(chosen));
-			int songsChosen = 0, totalTime = times[timeLim], largest = INT_MIN;
+		if(arr[timeLim].length == timeLim) {
+			int songsChosen = 0, minGap = INT_MAX, replace, replaceWith;
 			for(int i = numSongs - 1, j = timeLim; i >= 0; i--) {
-				if(table[i][j]) {
-					songsChosen++, chosen[i] = true;
-					if(songs[i] > largest)
-						largest = songs[i];
-					j -= songs[i];
+				if(map[i][j]) {
+					songsChosen++;
+					int curGap = INT_MAX, curReplaceWith;
+					for(int j = 0; j < numSongs; j++) {
+						int diff = songs[i].length - songs[j].length;
+						if(diff > 0 && diff < curGap)
+							curGap = diff, curReplaceWith = j;
+					}
+					if(curGap < minGap)
+						minGap = curGap, replace = i, replaceWith = curReplaceWith;
 				}
 			}
-			int secondLargest = INT_MIN;
-			for(int i = 0; i < numSongs; i++) {
-				if(!chosen[i] && songs[i] > secondLargest)
-					secondLargest = songs[i];
+
+			int totalTime = arr[timeLim].length - songs[replace].length + songs[replaceWith].length + LONG_SONG_TIME;
+
+			cout << "Case " << num++ << ": "
+				 << songsChosen << " " << totalTime << endl;
+		}
+		else {
+			int songsChosen = 0;
+			for(int i = numSongs - 1, j = timeLim; i >= 0; i--) {
+				if(map[i][j]) {
+					songsChosen++;
+					j -= songs[i].length;
+				}
 			}
 
-			if(totalTime == timeLim)
-				totalTime += JIN_GE_JIN_QU_TIME - largest + secondLargest;
-			else
-				totalTime += JIN_GE_JIN_QU_TIME, songsChosen++;
-
-			cout << "Case " << caseNum << ": " << songsChosen << " " << totalTime << endl;
+			cout << "Case " << num++ << ": "
+				 << songsChosen + 1 << " " << arr[timeLim].length + LONG_SONG_TIME << endl;
 		}
 	}
 }
