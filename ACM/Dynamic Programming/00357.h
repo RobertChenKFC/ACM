745b716b29c98458f1fcfbde716a1ea49a3a4b24
robert <robert.chen.kfc@gmail.com>
2016-08-26 17:09:49 +0800

    00357 solved

--- ACM/Dynamic Programming/00357.h
+++ ACM/Dynamic Programming/00357.h
@@ -1,32 +1,26 @@
+// Let Me Count The Ways *AC*
+
 #include <iostream>
-#include <cstring>
-#include <climits>
 
 using namespace std;
 
-const int MAX = 30005, NUM_CENTS = 5;
+const int MAX = 30005;
+const int NUM_COINS = 5;
+
+typedef long long int64;
 
 int main() {
-	int arr[MAX];
+	int64 dp[MAX] = { 0 };
+	dp[0] = 1;
 	int cents[] = { 1, 5, 10, 25, 50 };
+	for(int i = 0; i < NUM_COINS; i++) {
+		for(int j = cents[i]; j < MAX; j++)
+			dp[j] += dp[j - cents[i]];
+	}
 
-	int n;
-	while(cin >> n) {
-
-		memset(arr, MAX, sizeof(arr));
-		arr[0] = 0;
-		int m = 0;
-
-		for(int i = 0; i < NUM_CENTS; i++) {
-			for(int j = cents[i]; j <= n; j++) {
-				if(arr[j - cents[i]] + 1 < arr[j]) {
-					arr[j] = arr[j - cents[i]] + 1;
-					if(j == n)
-						m++;
-				}
-			}
-		}
-
-		cout << arr[n] << endl;
+	int money;
+	while(cin >> money) {
+		if(dp[money] == 1) cout << "There is only 1 way to produce " << money << " cents change." << endl;
+		else cout << "There are " << dp[money] << " ways to produce " << money << " cents change." << endl;
 	}
 }
--- ACM/main.cpp
+++ ACM/main.cpp
@@ -91,11 +91,11 @@
 /* Dynamic Programming */
 //#include "Dynamic Programming/MaxSubarray.h"
 //#include "Dynamic Programming/0_1_Knapsack.h"
-//#include "Dynamic Programming/00357.h" // Let Me Count The Ways
+#include "Dynamic Programming/00357.h" // Let Me Count The Ways *AC*
 //#include "Dynamic Programming/00562.h" // Dividing Coins *AC*
 //#include "Dynamic Programming/00900.h" // Brick Wall Patterns *AC*
 //#include "Dynamic Programming/12149.h" // Feynman *AC*
-#include "Dynamic Programming/12563.h" // Jin Ge Jin Qu Hao *AC*
+//#include "Dynamic Programming/12563.h" // Jin Ge Jin Qu Hao *AC*
 
 /* Longest Increasing Subsequence */
 //#include "LIS/LIS.h"
